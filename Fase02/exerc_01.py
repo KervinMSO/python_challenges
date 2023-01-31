8c9e2cc2db4c94751727c7d36b5e180f89ca41ea
Kervin Marlon <kervinmso@hotmail.com>
2023-01-31 16:42:19 -0300

    exerc 1 otimizado

--- Fase02/exerc_01.py
+++ Fase02/exerc_01.py
@@ -51,107 +51,60 @@ print('Labirinto Eureka!')
 
 nome_do_desafiante = input('Qual é o seu nome? ')
 
-vidas = int(5)
-game_over = int(0)
+vidas = 5
+game_over = 0
 
 while vidas > game_over:
 
     pergunta_1 = input(' \n(Valendo 1 vida) \n 1- A inspiração para o nome da empresa Senfio é a tecnologia Wireless que significa: \n\t 1) Sem Fio \n\t 2) Senfio \n\t 3) Cabos \n\t 4) Nenhuma das alternativas \n')
-
-    if pergunta_1 == '1':
-        vidas -= 1
-        print(f'Resposta Errada, suas vidas totais agora é: {vidas}.')
     
-    if pergunta_1 == '2':
+    if pergunta_1 == '1':
         vidas += 1
         print(f'Resposta Correta, suas vidas totais agora é: {vidas}.')
-        
-    if pergunta_1 == '3':
+    else: 
         vidas -= 1
         print(f'Resposta Errada, suas vidas totais agora é: {vidas}.')
-        
-    if pergunta_1 == '4':
-        vidas -= 1
-        print(f'Resposta Errada, suas vidas totais agora é: {vidas}.')
-
-    if pergunta_1 >= '5':
-        vidas == 0
-        print('Desculpe, opção invalida.')
-        break
 
     pergunta_2 = input(' \n(Valendo 2 vida) \n 2- Como vai a matemática? Responda, 77+33 =? \n\t 1) 100 \n\t 2) 110 \n\t 3) 010 \n\t 4) 001 \n')
 
-    if pergunta_2 == '1':
-        vidas -= 2
-        print(f'Resposta Errada, suas vidas totais agora é: {vidas}.')
-    
     if pergunta_2 == '2':
         vidas += 2
         print(f'Resposta Correta, suas vidas totais agora é: {vidas}.')
-        
-    if pergunta_2 == '3':
+    else:
         vidas -= 2
         print(f'Resposta Errada, suas vidas totais agora é: {vidas}.')
-        
-    if pergunta_2 == '4':
-        vidas -= 2
-        print(f'Resposta Errada, suas vidas totais agora é: {vidas}.')
-
-    if pergunta_2 >= '5':
-        vidas == 0
-        print('Desculpe, opção invalida.')
-        break
 
     pergunta_3 = input(f' \n(Valendo 3 vida) \n 3-Imagine que seu Pai tem 5 filhos, gaga, gege, gigi, gogo e ? \n\t 1) Gugu Liberato \n\t 2) Pintinho Amarelinho \n\t 3) Gugu \n\t 4) {nome_do_desafiante.capitalize()} \n')
 
-    if pergunta_3 == '1':
-        vidas -= 3
-        print(f'Resposta Errada, suas vidas totais agora é: {vidas}.')
-    
-    if pergunta_3 == '2':
-        vidas -= 3
-        print(f'Resposta Errada, suas vidas totais agora é: {vidas}.')
-        
-    if pergunta_3 == '3':
-        vidas -= 3
-        print(f'Resposta Errada, suas vidas totais agora é: {vidas}.')
-        
     if pergunta_3 == '4':
         vidas += 3
         print(f'Resposta Correta, suas vidas totais agora é: {vidas}.')
-
-    if pergunta_3 >= '5':
-        vidas == 0
-        print('Desculpe, opção invalida.')
-        break
+    else:
+        vidas -= 3
+        print(f'Resposta Errada, suas vidas totais agora é: {vidas}.')
 
     if vidas <= game_over:
         break
 
-    pergunta_final = input(' \nPergunta final- Muito bem, espero que você tenha acumulado vidas para a pergunta final. \n\t Se você deseja sair do labirinto Eureka e descobrir a verdade, qual pílula deve escolher? \n\t 1) RedPill \n\t 2) BluePill \n')
+    while True:
+        pergunta_final = input(' \nPergunta final- Muito bem, espero que você tenha acumulado vidas para a pergunta final. \n\t Se você deseja sair do labirinto Eureka e descobrir a verdade, qual pílula deve escolher? \n\t 1) RedPill \n\t 2) BluePill \n')
 
-# RedPill um laço de repetição deve retirar 1 vida ,a cada iteração, para o total de letras de seu nome. 
-    if pergunta_final == '1':
-        print('Então você escolheu a RedPill, perca 1 vida pra cada letra do seu nome.')
-        for element in nome_do_desafiante:
-            vidas -=1
-        break
-    
-# BluePill um laço de repetição deve retirar 1 vida por iteração até que seu score seja menor ou igual a zero. 
-    if pergunta_final == '2':
-        print('Você escolheu a BluePill, pessima escolha!')
-        while (vidas>0):
-            vidas-=1
-        break
-
-    if pergunta_final >= '3':
-        vidas == 0
-        print('Desculpe, opção invalida.')
-        break
+        if pergunta_final == '1':
+            print('Então você escolheu a RedPill, perca 1 vida pra cada letra do seu nome.')
+            for element in nome_do_desafiante:
+                vidas -=1
+            break
+        elif pergunta_final == '2':
+            print('Você escolheu a BluePill, pessima escolha!')
+            while (vidas>0):
+                vidas-=1
+            break
+        else:
+            print('Desculpe, opção invalida.')
+    break
 
 if vidas > game_over:
     print(f'Parabéns, você finalizou o jogo com o total de {vidas} vidas.')
-
 else:
     print('Game Over')
         
